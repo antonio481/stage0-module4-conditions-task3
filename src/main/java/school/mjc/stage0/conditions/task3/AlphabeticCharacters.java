8f8206b9e5b3df46f1d24cd2c77b47a2c75897ad
Anton Danilov <adanilovx@gmail.com>
2023-01-22 14:19:39 +0400

    Decision commit

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        character = Character.toLowerCase(character);
+        if(!(Character.UnicodeBlock.of(character).equals(Character.UnicodeBlock.BASIC_LATIN)))
+            System.out.println("wrong alphabet!");
+        else if(character=='a'||character=='e'||character=='y'||character=='u'||character=='o'||character=='i')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if (Character.UnicodeBlock.of(symbol).equals(Character.UnicodeBlock.BASIC_LATIN))
+            System.out.println("English");
+        else
+            System.out.println("Non English");
     }
 }
